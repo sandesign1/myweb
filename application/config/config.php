58a587ad0523e879754c3e4e51e577d8ae050462
sandesign1 <55090627+sandesign1@users.noreply.github.com>
2019-09-09 21:23:46 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/pintu-perkim.go.id';
+$config['base_url'] = 'https://github.com/sandesign1/perkim.github.io';
 
 /*
 |--------------------------------------------------------------------------
