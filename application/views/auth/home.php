b07a95bdcf635e51f0b18aa681d1b41b60735a8d
sandesign1 <55090627+sandesign1@users.noreply.github.com>
2019-09-09 20:09:29 +0700

    Demo Perkim

---
+++ application/views/auth/home.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Dinas Perkim | Halaman Utama</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,,500,600,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="<?= base_url('asset/') ?>css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="<?= base_url('asset/') ?>css/animate.css">
+
+    <link rel="stylesheet" href="<?= base_url('asset/') ?>css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?= base_url('asset/') ?>css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="<?= base_url('asset/') ?>css/magnific-popup.css">
+
+    <link rel="stylesheet" href="<?= base_url('asset/') ?>css/aos.css">
+
+    <link rel="stylesheet" href="<?= base_url('asset/') ?>css/ionicons.min.css">
+
+    <link rel="stylesheet" href="<?= base_url('asset/') ?>css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="<?= base_url('asset/') ?>css/jquery.timepicker.css">
+
+
+    <link rel="stylesheet" href="<?= base_url('asset/') ?>css/flaticon.css">
+    <link rel="stylesheet" href="<?= base_url('asset/') ?>css/icomoon.css">
+    <link rel="stylesheet" href="<?= base_url('asset/') ?>css/style.css">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+        <div class="container">
+            <a class="navbar-brand" href="index.html"><img width="50px" src="<?= base_url('asset/') ?>images/logo.png" alt=""> DINAS PERKIM OKU TIMUR</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="oi oi-menu"></span> Menu
+            </button>
+
+            <div class="collapse navbar-collapse" id="ftco-nav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active"><a href="" class="nav-link">Home</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Profile Perkim</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Arsip Kegiatan</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Dokumen</a></li>
+                    <li class="nav-item cta"><a href="" class="nav-link"><span>Colorfull Perkim 2019</span></a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- END nav -->
+
+    <div class="hero-wrap js-fullheight">
+        <div class="overlay"></div>
+        <div class="container-fluid px-0">
+            <div class="row d-md-flex no-gutters slider-text align-items-center js-fullheight justify-content-end">
+                <img class="one-third js-fullheight align-self-end order-md-last img-fluid" src="<?= base_url('asset/') ?>images/undraw_pair_programming_njlp.svg" alt="">
+                <div class="one-forth d-flex align-items-center ftco-animate js-fullheight">
+                    <div class="text mt-5">
+                        <span class="subheading">Dinas Perkim OKU Timur Mengucapkan</span>
+                        <h1 class="mb-3"><span>Selamat</span> <span>Datang,</span></h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, quae!</p>
+                        <p><a href="#" class="btn btn-primary px-4 py-3">Lihat Arsip Kegiatan</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section class="ftco-section services-section bg-light">
+        <div class="container">
+            <div class="row justify-content-center mb-5 pb-3">
+                <div class="col-md-7 text-center heading-section ftco-animate">
+                    <h2 class="mb-4">Rencana Kegiatan Bulan ini</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In quasi non eum, optio modi impedit?</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 d-flex align-self-stretch ftco-animate">
+                    <div class="media block-6 services d-flex align-items-center">
+                        <div class="icon d-flex align-items-center justify-content-center order-md-last">
+                            <span class="flaticon-cloud"></span>
+                        </div>
+                        <div class="media-body pl-4 pl-md-0 pr-md-4 text-md-right">
+                            <h3 class="heading">Perumahan : Pembangunan Rumah di Trukis Barat</h3>
+                            <p class="mb-0">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea reiciendis voluptate qui repudiandae.</p><br>
+                            <p><a href="#" class="btn btn-danger px-4 py-2">selengkapnya...</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex align-self-stretch ftco-animate">
+                    <div class="media block-6 services d-flex align-items-center">
+                        <div class="icon d-flex align-items-center justify-content-center">
+                            <span class="flaticon-server"></span>
+                        </div>
+                        <div class="media-body pl-4">
+                            <h3 class="heading">Perumahan : Pembangunan Rumah di Trukis Barat</h3>
+                            <p class="mb-0">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea reiciendis voluptate qui repudiandae.</p><br>
+                            <p><a href="#" class="btn btn-danger px-4 py-2">selengkapnya...</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex align-self-stretch ftco-animate">
+                    <div class="media block-6 services d-flex align-items-center">
+                        <div class="icon d-flex align-items-center justify-content-center order-md-last">
+                            <span class="flaticon-customer-service"></span>
+                        </div>
+                        <div class="media-body pl-4 pl-md-0 pr-md-4 text-md-right">
+                            <h3 class="heading">Perumahan : Pembangunan Rumah di Trukis Barat</h3>
+                            <p class="mb-0">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea reiciendis voluptate qui repudiandae.</p><br>
+                            <p><a href="#" class="btn btn-danger px-4 py-2">selengkapnya...</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex align-self-stretch ftco-animate">
+                    <div class="media block-6 services d-flex align-items-center">
+                        <div class="icon d-flex align-items-center justify-content-center">
+                            <span class="flaticon-life-insurance"></span>
+                        </div>
+                        <div class="media-body pl-4">
+                            <h3 class="heading">Perumahan : Pembangunan Rumah di Trukis Barat</h3>
+                            <p class="mb-0">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea reiciendis voluptate qui repudiandae.</p><br>
+                            <p><a href="#" class="btn btn-danger px-4 py-2">selengkapnya...</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex align-self-stretch ftco-animate">
+                    <div class="media block-6 services d-flex align-items-center">
+                        <div class="icon d-flex align-items-center justify-content-center order-md-last">
+                            <span class="flaticon-cloud-computing"></span>
+                        </div>
+                        <div class="media-body pl-4 pl-md-0 pr-md-4 text-md-right">
+                            <h3 class="heading">Perumahan : Pembangunan Rumah di Trukis Barat</h3>
+                            <p class="mb-0">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea reiciendis voluptate qui repudiandae.</p><br>
+                            <p><a href="#" class="btn btn-danger px-4 py-2">selengkapnya...</a></p>
+                        </div>s
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex align-self-stretch ftco-animate">
+                    <div class="media block-6 services d-flex align-items-center">
+                        <div class="icon d-flex align-items-center justify-content-center">
+                            <span class="flaticon-settings"></span>
+                        </div>
+                        <div class="media-body pl-4">
+                            <h3 class="heading">Perumahan : Pembangunan Rumah di Trukis Barat</h3>
+                            <p class="mb-0">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea reiciendis voluptate qui repudiandae.</p><br>
+                            <p><a href="#" class="btn btn-danger px-4 py-2">selengkapnya...</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="ftco-section ftco-counter img" id="section-counter">
+        <div class="container">
+            <div class="row justify-content-center mb-5">
+                <div class="col-md-7 text-center heading-section heading-section-white ftco-animate">
+                    <span class="subheading">Rencana Kegiatan Perkim Periode 2019</span>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-10">
+                    <div class="row">
+                        <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+                            <div class="block-18 text-center">
+                                <div class="text">
+                                    <strong class="number" data-number="12">0</strong>
+                                    <span>Divisi Perumahan</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+                            <div class="block-18 text-center">
+                                <div class="text">
+                                    <strong class="number" data-number="9">0</strong>
+                                    <span>Divisi Permukiman</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+                            <div class="block-18 text-center">
+                                <div class="text">
+                                    <strong class="number" data-number="9">0</strong>
+                                    <span>Divisi Permukiman</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 d-flex justify-content-center counter-wrap ftco-animate">
+                            <div class="block-18 text-center">
+                                <div class="text">
+                                    <strong class="number" data-number="9">0</strong>
+                                    <span>Divisi Permukiman</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer class="ftco-footer ftco-bg-dark ftco-section">
+        <div class="container">
+            <div class="row mb-5">
+                <div class="col-md">
+                    <div class="ftco-footer-widget mb-4 bg-primary p-4">
+                        <h2 class="ftco-heading-2">CoHost</h2>
+                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                        <ul class="ftco-footer-social list-unstyled mb-0">
+                            <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                            <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                            <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="ftco-footer-widget mb-4 ml-md-5">
+                        <h2 class="ftco-heading-2">Unseful Links</h2>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="py-2 d-block">Servers</a></li>
+                            <li><a href="#" class="py-2 d-block">Windos Hosting</a></li>
+                            <li><a href="#" class="py-2 d-block">Cloud Hosting</a></li>
+                            <li><a href="#" class="py-2 d-block">OS Servers</a></li>
+                            <li><a href="#" class="py-2 d-block">Linux Servers</a></li>
+                            <li><a href="#" class="py-2 d-block">Policy</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="ftco-footer-widget mb-4">
+                        <h2 class="ftco-heading-2">Navigational</h2>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="py-2 d-block">Home</a></li>
+                            <li><a href="#" class="py-2 d-block">Domain</a></li>
+                            <li><a href="#" class="py-2 d-block">Hosting</a></li>
+                            <li><a href="#" class="py-2 d-block">About</a></li>
+                            <li><a href="#" class="py-2 d-block">Blog</a></li>
+                            <li><a href="#" class="py-2 d-block">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="ftco-footer-widget mb-4">
+                        <h2 class="ftco-heading-2">Office</h2>
+                        <div class="block-23 mb-3">
+                            <ul>
+                                <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
+                                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
+                                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 text-center">
+
+                    <p>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        Copyright &copy;<script>
+                            document.write(new Date().getFullYear());
+                        </script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+
+    <!-- loader -->
+    <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
+            <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+            <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00" /></svg></div>
+
+
+    <script src="<?= base_url('asset/') ?>js/jquery.min.js"></script>
+    <script src="<?= base_url('asset/') ?>js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="<?= base_url('asset/') ?>js/popper.min.js"></script>
+    <script src="<?= base_url('asset/') ?>js/bootstrap.min.js"></script>
+    <script src="<?= base_url('asset/') ?>js/jquery.easing.1.3.js"></script>
+    <script src="<?= base_url('asset/') ?>js/jquery.waypoints.min.js"></script>
+    <script src="<?= base_url('asset/') ?>js/jquery.stellar.min.js"></script>
+    <script src="<?= base_url('asset/') ?>js/owl.carousel.min.js"></script>
+    <script src="<?= base_url('asset/') ?>js/jquery.magnific-popup.min.js"></script>
+    <script src="<?= base_url('asset/') ?>js/aos.js"></script>
+    <script src="<?= base_url('asset/') ?>js/jquery.animateNumber.min.js"></script>
+    <script src="<?= base_url('asset/') ?>js/bootstrap-datepicker.js"></script>
+    <script src="<?= base_url('asset/') ?>js/jquery.timepicker.min.js"></script>
+    <script src="<?= base_url('asset/') ?>js/scrollax.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+    <script src="<?= base_url('asset/') ?>js/google-map.js"></script>
+    <script src="<?= base_url('asset/') ?>js/main.js"></script>
+
+</body>
+
+</html>
